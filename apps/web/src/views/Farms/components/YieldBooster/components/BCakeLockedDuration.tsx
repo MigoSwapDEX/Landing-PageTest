904556ac11be3b450595bd1b489e5b814821345b
Olotu Oluwagbemiga <gbenga.isess@gmail.com>
2023-03-20 11:46:35 +0100

    Minor updates to the BCakeLockedDuration

--- apps/web/src/views/Farms/components/YieldBooster/components/BCakeLockedDuration.tsx
+++ apps/web/src/views/Farms/components/YieldBooster/components/BCakeLockedDuration.tsx
@@ -62,7 +62,7 @@ const LockDurationField: React.FC<React.PropsWithChildren<LockDurationFieldProps
                 pr="12px"
                 scale="sm"
                 disabled={weekSeconds > maxAvailableDuration}
-                variant={weekSeconds === duration ? 'subtle' : 'dark'}
+                variant={weekSeconds === duration ? 'subtle' : 'light'}
               >
                 {week}W
               </Button>
